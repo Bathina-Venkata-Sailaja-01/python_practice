1db942ba8127271dd4921b07037786ef0d2f41ab
B Venkata Sailaja <bathinavenkatasailaja@gmail.com>
2026-07-25 20:48:03 +0530

    Day 3: Learned about type Casting)

---
+++ Day03/README.md
@@ -0,0 +1,22 @@
+## Type cast
+- Changing one data type to another data type
+- how to convert integer to float and int to string and int to bool vice versa
+
+## Boolean TypeCasting
+Examples:
+- bool(100)    # True
+- bool(-5)     # True
+- bool(0.1)    # True
+- bool(0)      # False
+
+**Python treats these values as False:**
+
+- 0
+- 0.0
+- 0j
+- None
+- Empty strings ""
+- Empty lists []
+- Empty tuples ()
+- Empty dictionaries {}
+- Empty sets set()
---
+++ Day03/typecasting.py
@@ -0,0 +1,32 @@
+#======================================
+# 1.Type Casting
+#======================================
+# Integer Type Casting
+#--------------------------------------
+num=10
+print(type(10))
+print(float(num))
+print(str(num))
+print(bool(num))#it prints "True".This happens because any non-zero number is considered True in Python.
+#-------------------------------------
+# Float Type Casting
+#-------------------------------------
+num1=10.0
+print(int(num1))
+print(str(num1))#if keep print statement they we don't get thee inverted commas in output
+print(bool(num1))
+#------------------------------------
+# String Type Casting
+#------------------------------------
+str1='Sailu'
+#print(int(str1))#Value Error
+#print(float(str1))#Value Error
+print(bool(str1))
+#-----------------------------------
+# Boolean
+#-----------------------------------
+print(bool('10'))
+print(bool('10.0'))
+print(bool('apple'))
+print(bool(''))
+print(bool(' '))
